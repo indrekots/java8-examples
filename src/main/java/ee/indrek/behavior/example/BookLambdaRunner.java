1dbcf1adf3688e1b9e739f4824778cd114f6a182
Indrek Ots <ots.indrek@gmail.com>
2015-10-26 19:14:00 +0200

    optimize imports

--- src/main/java/ee/indrek/behavior/example/BookLambdaRunner.java
+++ src/main/java/ee/indrek/behavior/example/BookLambdaRunner.java
@@ -3,7 +3,6 @@ package ee.indrek.behavior.example;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toList;
 
