e90ec3e1167c126113b1fa6e882c70a25eea5347
Indrek Ots <ots.indrek@gmail.com>
2015-11-15 14:11:55 +0200

    find 2011 transactions and sort them by value

--- src/main/java/ee/indrek/traderstransactions/Runner.java
+++ src/main/java/ee/indrek/traderstransactions/Runner.java
@@ -3,10 +3,22 @@ package ee.indrek.traderstransactions;
 import java.util.Arrays;
 import java.util.List;
 
+import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.toList;
+
 public class Runner {
 
     public static void main(String[] args) {
         List<Transaction> transactions = createTransactions();
+
+        System.out.println(transactionsOfYear2011(transactions));
+    }
+
+    private static List<Transaction> transactionsOfYear2011(List<Transaction> transactions) {
+        return transactions.stream()
+                .filter(t -> t.getYear() == 2011)
+                .sorted(comparing(Transaction::getValue))
+                .collect(toList());
     }
 
     private static List<Transaction> createTransactions() {
