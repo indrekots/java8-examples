5ec785ebc86c2d46d4b576b827f286b00e0a550f
Indrek Ots <ots.indrek@gmail.com>
2016-02-18 22:18:30 +0200

    instead of multiply, sum the numbers

--- src/main/java/ee/indrek/forkjoin/ForkJoinExample.java
+++ src/main/java/ee/indrek/forkjoin/ForkJoinExample.java
@@ -1,5 +1,13 @@
 package ee.indrek.forkjoin;
 
-public class ForkJoinExample {
+import java.util.concurrent.ForkJoinPool;
+import java.util.stream.LongStream;
 
+public class ForkJoinExample {
+    public static void main(String[] args) {
+        long[] numbers = LongStream.rangeClosed(1, 10000).toArray();
+        LongSum multiplier = new LongSum(numbers, 0, numbers.length);
+        Long result = new ForkJoinPool().invoke(multiplier);
+        System.out.println(result);
+    }
 }
