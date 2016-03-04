930f5ba89641fc61b4584bbd84cf2af19a3fea54
Indrek Ots <ots.indrek@gmail.com>
2016-03-04 07:49:45 +0200

    implement a simpmle example of an optional

--- src/main/java/ee/indrek/optional/OptionalExample.java
+++ src/main/java/ee/indrek/optional/OptionalExample.java
@@ -1,7 +1,17 @@
 package ee.indrek.optional;
 
-/**
- * Created by indrek on 4.03.16.
- */
+import ee.indrek.behavior.example.Book;
+
+import java.util.Optional;
+
 public class OptionalExample {
+
+    public static void main(String[] args) {
+        Optional<Book> book = findBook("The War of the Worlds");
+        Book b = book.get();
+    }
+
+    private static Optional<Book> findBook(String title) {
+        return null;
+    }
 }
