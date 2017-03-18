59a16e48510bc1fcf6de316bc1f5d8e81677b4a0
Indrek Ots <ots.indrek@gmail.com>
2017-03-18 19:24:04 +0200

    Example of using java.time.Clock

--- src/main/java/ee/indrek/time/ReplaceableClockDemo.java
+++ src/main/java/ee/indrek/time/ReplaceableClockDemo.java
@@ -1,12 +1,23 @@
 package ee.indrek.time;
 
 import java.time.Clock;
+import java.time.LocalDateTime;
 
-public class ReplaceableClock {
+public class ReplaceableClockDemo {
 
-    private Clock clock;
+    public static void main(String[] args) {
+        ReplaceableClockDemo demo = new ReplaceableClockDemo(Clock.systemUTC());
+        System.out.println(demo.isNowBefore(LocalDateTime.of(2017, 3, 1, 12, 0)));
+    }
 
-    public ReplaceableClock(Clock clock) {
+    private final Clock clock;
+
+    public ReplaceableClockDemo(Clock clock) {
         this.clock = clock;
     }
+
+    public boolean isNowBefore(LocalDateTime dateTime) {
+        return LocalDateTime.now(clock).isBefore(dateTime);
+    }
+
 }
--- src/test/java/ee/indrek/time/ReplaceableClockDemoTest.java
+++ src/test/java/ee/indrek/time/ReplaceableClockDemoTest.java
@@ -1,8 +1,32 @@
-import static org.junit.Assert.*;
+package ee.indrek.time;
 
-/**
- * Created by indrek on 28.02.17.
- */
+import org.junit.Test;
+
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class ReplaceableClockDemoTest {
 
+    @Test
+    public void shouldBeBeforeStPatricksDay2017() throws Exception {
+        Clock now = Clock.fixed(LocalDateTime.of(2017, 2, 15, 12, 34)
+                .toInstant(ZoneOffset.UTC), ZoneId.of("UTC"));
+        LocalDateTime stPatricksDay = LocalDateTime.of(2017, 3, 17, 0, 0);
+        ReplaceableClockDemo demo = new ReplaceableClockDemo(now);
+        assertTrue(demo.isNowBefore(stPatricksDay));
+    }
+
+    @Test
+    public void shouldNotBeBeforePiDay2017() throws Exception {
+        Clock now = Clock.fixed(LocalDateTime.of(2017, 5, 18, 9, 45)
+                .toInstant(ZoneOffset.UTC), ZoneId.of("UTC"));
+        LocalDateTime piDay = LocalDateTime.of(2017, 3, 14, 0, 0);
+        ReplaceableClockDemo demo = new ReplaceableClockDemo(now);
+        assertFalse(demo.isNowBefore(piDay));
+    }
 }
